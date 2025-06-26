863f61ad5dbb938d6fa6a26b0afc3fea482a1a5c
Raja Mahin <raja.mahin20@gmail.com>
2025-06-26 11:55:07 +0500

    Refactoring Logic into an entity state

---
+++ Wpm.Management.Domain/Entity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpm.Management.Domain
+{
+    public class Entity : IEquatable<Entity>
+    {
+        public Guid Id { set; get; }
+
+
+        public bool Equals(Entity? other)
+        {
+            return other?.Id == Id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public static bool operator ==(Entity? right, Entity Left)
+        {
+            return Left?.Id == right?.Id;
+        }
+
+        public static bool operator !=(Entity? right, Entity Left)
+        {
+            return Left?.Id != right?.Id;
+        }
+
+    }
+
+}
--- Wpm.Management.Domain/Pet.cs
+++ Wpm.Management.Domain/Pet.cs
@@ -1,50 +1,29 @@
 ﻿namespace Wpm.Management.Domain;
 
-public class Pet : IEquatable<Pet>
+
+
+/* USING int be generally avoided in Domain Driven Design, 
+   Using Guid for entity IDs avoids exposing predictable sequences,
+  supports global uniqueness across distributed systems, 
+  and aligns with the domain’s conceptual identity.
+
+  In contrast, int IDs tie your domain model to database implementation details, 
+  risking collisions and leaking sensitive growth patterns
+
+  public int Id { set; get; }
+
+  */
+
+
+
+
+
+public class Pet : Entity
 {
-    /* USING int be generally avoided in Domain Driven Design, 
-     Using Guid for entity IDs avoids exposing predictable sequences,
-    supports global uniqueness across distributed systems, 
-    and aligns with the domain’s conceptual identity.
-    
-    In contrast, int IDs tie your domain model to database implementation details, 
-    risking collisions and leaking sensitive growth patterns
-    
-    public int Id { set; get; }
-
-    */
-
-    public Guid Id { set; get; }
 
     public string? Name { set; get; }
 
     public int Age { set; get; }
-
-
-    public bool Equals(Pet? other)
-    {
-        return other?.Id == Id;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        return base.Equals(obj);
-    }
-
-    public override int GetHashCode()
-    {
-        return Id.GetHashCode();
-    }
-
-    public static bool operator ==(Pet? right, Pet Left)
-    {
-        return Left?.Id == right?.Id;
-    }
-
-    public static bool operator !=(Pet? right, Pet Left)
-    {
-        return Left?.Id != right?.Id;
-    }
 
 
 }
