e65e201b973e038532e75c1618e258fe2de9a0ce
Raja Mahin Zaman <62209180+RajaMahin@users.noreply.github.com>
2025-07-15 16:20:32 +0500

    Adding AggregateRoot and SharedKernel

--- Wpm.SharedKernel/Entity.cs
+++ Wpm.SharedKernel/Entity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Wpm.Management.Domain
+﻿namespace Wpm.SharedKernel
 {
     public class Entity : IEquatable<Entity>
     {
